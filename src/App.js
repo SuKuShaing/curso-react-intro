e4be8114b59eac07068a306ed9244918933f03a2
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-12-15 18:21:14 -0300

    Mi primer componente wiiii

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './platzi.webp';
-import './App.css';
+import logo from "./platzi.webp";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	return (
+		<div className="App">
+			<TodoItem />
+			<TodoItem />
+			<TodoItem />
+
+      <TodoCount />
+
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edita el archivo <code>src/App.js</code> y guarda para recargar.
+				</p>
+				<a
+					className="App-link"
+					href="https://platzi.com/reactjs"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Aprendamos React
+				</a>
+			</header>
+		</div>
+	);
+}
+
+function TodoCount() {
+	return (
+    <hl>
+      Completaste
+      3 de 
+      5 TODOs
+    </hl>
   );
+}
+
+function TodoItem() {
+	return (
+		<li>
+			<span>V</span>
+			<p>Llorar con la Llorona</p>
+			<span>X</span>
+		</li>
+	);
 }
 
 export default App;
