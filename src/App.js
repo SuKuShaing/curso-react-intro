c605621f78bbd8329a751ed3e4f02331a29f0076
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-12-16 14:13:27 -0300

    se envían parametros o mejor dicho propiedades, las props entre componentes

--- src/App.js
+++ src/App.js
@@ -3,23 +3,34 @@ import { TodoSearch } from "./TodoSearch";
 import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
+import React from "react";
 
 import "./App.css";
 
+const defaultTodos = [
+	{ text: "Cortar cebolla", completed: true },
+	{ text: "Tomar el curso de introducción a React.js", completed: true },
+	{ text: "Llorar con la llorona", completed: true },
+	{ text: "Otra tarea", completed: true },
+	{ text: "Cortar el pasto", completed: true },
+	{ text: "barrer la calle", completed: true },
+]
+
 function App() {
 	return (
-		<div className="App">
+		<React.Fragment>
 			<TodoCounter completed={16} total={25} />
 			<TodoSearch />
 
 			<TodoList>
-				<TodoItem />
-				<TodoItem />
-				<TodoItem />
+				{/* React necesita una key cuando itera desde un array */}
+				{defaultTodos.map((props) => (
+					<TodoItem key={props.text} text={props.text} completed={props.completed} />
+				))}
 			</TodoList>
 
 			<CreateTodoButton />
-		</div>
+		</React.Fragment>
 	);
 }
 
