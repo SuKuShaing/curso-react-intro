4451c4ad00785e0837e112f4f0dcd29a7462b077
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-12-16 13:39:17 -0300

    Añade componentes de lista de tareas: TodoCounter, TodoSearch, TodoList y TodoItem

--- src/App.js
+++ src/App.js
@@ -1,50 +1,24 @@
-import logo from "./platzi.webp";
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
+
 import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
-			<TodoItem />
-			<TodoItem />
-			<TodoItem />
+			<TodoCounter />
+			<TodoSearch />
 
-      <TodoCount />
+			<TodoList>
+				<TodoItem />
+				<TodoItem />
+				<TodoItem />
+			</TodoList>
 
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edita el archivo <code>src/App.js</code> y guarda para recargar.
-				</p>
-				<a
-					className="App-link"
-					href="https://platzi.com/reactjs"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Aprendamos React
-				</a>
-			</header>
+			{/* <CreateTodoButton /> */}
 		</div>
-	);
-}
-
-function TodoCount() {
-	return (
-    <hl>
-      Completaste
-      3 de 
-      5 TODOs
-    </hl>
-  );
-}
-
-function TodoItem() {
-	return (
-		<li>
-			<span>V</span>
-			<p>Llorar con la Llorona</p>
-			<span>X</span>
-		</li>
 	);
 }
 
