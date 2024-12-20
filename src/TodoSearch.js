b1c6e84a3b18eaefb2739b913ec9e775cae72ec2
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-12-20 13:17:32 -0300

    manejo de estado en react

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,15 +1,24 @@
+import React from "react";
 import "./TodoSearch.css";
 
+
 function TodoSearch() {
+	// const [state, setState] = React.useState('Valor inicial del estado');
+	// state es inmutable (no se puede modificar directamente)
+	// setState es el que modifica el estado, ambos pueden tener el nombre que quieran
+
+	const [searchValue, setSearchValue] = React.useState('');
+
+	console.log('Los usuarios buscan todos de ' + searchValue);
+
 	return (
 		<input
 			placeholder="Cortar Cebolla"
 			className="input-TodoSearch"
+			value={searchValue} // valor que van a ver los usuarios
 			onChange={(event) => {
-					console.log("Eventos del input");
-					console.log(event);
-					console.log(event.target);
 					console.log(event.target.value);
+					setSearchValue(event.target.value);
 				}}
 		/>
 	);
