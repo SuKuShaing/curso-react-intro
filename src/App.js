cc26775d1d0f45d58da22ab651d2a6c86969caf9
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-12-27 14:57:58 -0300

    Actualizar gestión de tareas: almacenar y recuperar tareas desde localStorage

--- src/App.js
+++ src/App.js
@@ -9,20 +9,33 @@ import { ContainerWhite } from "./ContainerWhite";
 
 import "./App.css";
 
-const defaultTodos = [
-	{ text: "Cortar cebolla", completed: false },
-	{ text: "Tomar el curso de introducción a React.js", completed: false },
-	{ text: "Llorar con la llorona", completed: false },
-	{ text: "Otra tarea", completed: false },
-	{ text: "Cortar el pasto", completed: false },
-	{ text: "barrer la calle", completed: false },
-	{ text: "Aprender estados", completed: true },
-	{ text: "Usar estados deribados", completed: true },
-];
+// const defaultTodos = [
+// 	{ text: "Cortar cebolla", completed: false },
+// 	{ text: "Tomar el curso de introducción a React.js", completed: false },
+// 	{ text: "Llorar con la llorona", completed: false },
+// 	{ text: "Otra tarea", completed: false },
+// 	{ text: "Cortar el pasto", completed: false },
+// 	{ text: "barrer la calle", completed: false },
+// 	{ text: "Aprender estados", completed: true },
+// 	{ text: "Usar estados deribados", completed: true },
+// ];
+
+// localStorage.setItem('todosEnLocalStorage', JSON.stringify(defaultTodos)); // guardamos los todos en el localStorage
 
 function App() {
+	const localStorageTodos = localStorage.getItem('todosEnLocalStorage'); // obtenemos los todos del localStorage
+
+	let parsedTodos; // declaramos una variable para guardar los todos del localStorage
+
+	if (!localStorageTodos) {
+		localStorage.setItem('todosEnLocalStorage', JSON.stringify([])); // si no hay todos en el localStorage, creamos un array vacío
+		parsedTodos = [];
+	} else {
+		parsedTodos = JSON.parse(localStorageTodos); // convertimos los todos del localStorage a un array de objetos
+	}
+
 	// Estados
-	const [todos, setTodos] = React.useState(defaultTodos); // al estado de Todos le pasamos el array de defaultTodos
+	const [todos, setTodos] = React.useState(parsedTodos); // al estado de Todos le pasamos el array de defaultTodos
 	const [searchValue, setSearchValue] = React.useState('');
 	console.log('Los usuarios buscan todos de ' + searchValue);
 
@@ -36,6 +49,11 @@ function App() {
 	});
 
 	// Funciones
+	const iSaveTodos = (newTodos) => { // función para guardar los todos en el localStorage y actualizar el estado de todos
+		localStorage.setItem('todosEnLocalStorage', JSON.stringify(newTodos)); // guardamos los todos en el localStorage
+		setTodos(newTodos); // setTodos es la función que actualiza el estado de todos
+	}
+
 	const iCompletedTodo = (text) => {
 		const newTodos = [...todos];
 		const todoIndex = newTodos.findIndex(
@@ -46,7 +64,7 @@ function App() {
 		} else {
 			newTodos[todoIndex].completed = true;
 		}
-		setTodos(newTodos); // setTodos es la función que actualiza el estado de todos
+		iSaveTodos(newTodos);
 	};
 
 	const iDeleteTodo = (text) => {
@@ -55,7 +73,7 @@ function App() {
 			(todo) => todo.text === text
 		);
 		newTodos.splice(todoIndex, 1); // splice elimina un elemento de un array en el indice que le indiquemos y avanza la cantidad de elementos que le indiquemos
-		setTodos(newTodos); // setTodos es la función que actualiza el estado de todos
+		iSaveTodos(newTodos);
 	};
 
 	return (
