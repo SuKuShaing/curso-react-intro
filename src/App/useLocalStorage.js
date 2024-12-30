a36729462ac3f4963aab02186c6d308a5b81cb8e
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-12-30 13:24:13 -0300

    Agregar manejo de estado de carga y error en el Hook useLocalStorage

--- src/App/useLocalStorage.js
+++ src/App/useLocalStorage.js
@@ -2,23 +2,33 @@ import React from "react";
 
 // Custom Hook
 // Hook personalizado para guardar los todos en el localStorage
-function useLocalStorage(itemName, [initialValue]) {
+function useLocalStorage(itemName, initialValue) {
 	const [item, setItem] = React.useState(initialValue); // declaramos un estado para guardar los todos
 	const [loading, setLoading] = React.useState(true); // declaramos un estado para saber si los todos se están cargando
 	const [error, setError] = React.useState(false); // declaramos un estado para saber si hubo un error al cargar los todos
 
 	React.useEffect(() => {
-		const localStorageItem = localStorage.getItem(itemName); // obtenemos los todos del localStorage
+		setTimeout(() => {
+			try {
+				const localStorageItem = localStorage.getItem(itemName); // obtenemos los todos del localStorage
 
-		let parsedItem; // declaramos una variable para guardar los todos del localStorage
+				let parsedItem; // declaramos una variable para guardar los todos del localStorage
 
-		if (!localStorageItem) {
-			localStorage.setItem(itemName, JSON.stringify(initialValue)); // si no hay todos en el localStorage, creamos un valor inicial que nos enviaron
-			parsedItem = initialValue;
-		} else {
-			parsedItem = JSON.parse(localStorageItem); // convertimos los todos del localStorage a un array de objetos
-		}
-	});
+				if (!localStorageItem) {
+					localStorage.setItem(itemName, JSON.stringify(initialValue)); // si no hay todos en el localStorage, creamos un valor inicial que nos enviaron
+					parsedItem = initialValue;
+				} else {
+					parsedItem = JSON.parse(localStorageItem); // convertimos los todos del localStorage a un array de objetos
+					setItem(parsedItem); // setTodos es la función que actualiza el estado de todos
+				}
+
+				setLoading(false); // cambiamos el estado de loading a false
+			} catch (error) {
+				setLoading(false); // cambiamos el estado de loading a false
+				setError(true); // si hay un error al cargar los todos, cambiamos el estado de error a true
+			}
+		}, 2000);
+	}, []); // si le pasamos un array vacio, el useEffect se ejecutará solo una vez
 
 	const iSaveItem = (newItem) => {
 		// función para guardar los todos en el localStorage y actualizar el estado de todos
@@ -30,7 +40,7 @@ function useLocalStorage(itemName, [initialValue]) {
 		item,
 		iSaveItem,
 		loading,
-		error
+		error,
 	}; // retornamos los estados y la función para guardar los todos
 }
 
